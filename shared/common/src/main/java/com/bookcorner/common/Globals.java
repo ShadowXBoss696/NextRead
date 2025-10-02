6f33d37e869ce273f273e2b7f48cfe4a02507d27
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2025-10-02 22:36:59 +0530

    Add initial Gradle build configuration and main application class

--- shared/common/src/main/java/com/bookcorner/common/Globals.java
+++
@@ -1,6 +0,0 @@
-package com.bookcorner.common;
-
-public class Globals {
-
-    public static final String BASE_PACKAGE = "com.bookcorner";
-}
---
+++ src/main/java/com/bookcorner/demo/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.bookcorner.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
